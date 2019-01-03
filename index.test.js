f71bc1afa4f890168115d3303381786c1103b2bc
GeekKorky <kaori03252014@gmail.com>
2019-01-03 19:48:39 +0800

    add multiply function

--- index.test.js
+++ index.test.js
@@ -1,7 +1,12 @@
-const sum = require("./index");
+const { sum, multiply } = require("./index");
 
 test("adds 1 + 2 to equal 3", sumTest);
+test("multiply 1 * 2 equal 2", multiplyTest);
 
 function sumTest() {
   expect(sum(1, 2)).toBe(3);
+}
+
+function multiplyTest() {
+  expect(multiply(1, 2)).toBe(2);
 }
